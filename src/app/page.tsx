725ef116a5fd5f529004acb9b58ee194841be4d0
Aaron Nolan <nolan_aaron@outlook.com>
2023-10-21 00:24:12 +1000

    Core logic work in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,81 @@
 "use client";
+import { ChakraProvider, Button } from "@chakra-ui/react";
+import InputWithButton from "./components/InputWithButton";
+import ToggleButton from "./components/ToggleButton";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
+  const [numbers, setNumbers] = useState<{ [key: number]: number }>({});
+
+  useEffect(() => {
+    console.log(numbers);
+  }, [numbers]);
+
+  const setTimer = (inputValue: string) => {
+    const numberValue = parseFloat(inputValue);
+
+    if (isNaN(numberValue) || numberValue <= 0) {
+      console.log("Invalid input");
+      return;
+    }
+
+    setButtonDisabled(!buttonDisabled);
+  };
+
+  const addNumber = (inputValue: string) => {
+    const numberValue = parseFloat(inputValue);
+
+    if (isNaN(numberValue) || numberValue < 0) {
+      console.log("Invalid input");
+      return;
+    }
+
+    setNumbers((prevNumbers) => {
+      const updatedNumbers = { ...prevNumbers };
+      if (updatedNumbers.hasOwnProperty(numberValue)) {
+        updatedNumbers[numberValue] += 1;
+      } else {
+        updatedNumbers[numberValue] = 1;
+      }
+      return updatedNumbers;
+    });
+  };
+
+  const listNumbers = () => {
+    const numberEntries = Object.entries(numbers);
+
+    numberEntries.map(([key, value]) =>
+      console.log(`Number: ${key}, Quantity: ${value}`)
+    );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="max-w-5xl w-full items-center justify-between font-mono lg:flex">
-        <h1>Fibly</h1>
-      </div>
-    </main>
+    <ChakraProvider>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="max-w-5xl w-full font-mono lg:flex">
+          <InputWithButton
+            buttonText="Set"
+            buttonColour="green"
+            inputPlaceholder="Set interval time (seconds)"
+            inputType="number"
+            disabled={buttonDisabled}
+            handleClick={setTimer}
+          />
+          <InputWithButton
+            buttonText="Add"
+            buttonColour="purple"
+            inputPlaceholder="Input a number"
+            inputType="number"
+            handleClick={addNumber}
+          />
+          <ToggleButton firstOption="Resume" secondOption="Halt" />
+          <Button colorScheme="red" onClick={listNumbers}>
+            Quit
+          </Button>
+        </div>
+      </main>
+    </ChakraProvider>
   );
 };
 
