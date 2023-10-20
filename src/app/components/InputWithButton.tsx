b8898e16862d8f2af79ec92b8235247e84d82fbb
Aaron Nolan <nolan_aaron@outlook.com>
2023-10-21 09:43:58 +1000

    Input buttons now trigger on enter key

--- src/app/components/InputWithButton.tsx
+++ src/app/components/InputWithButton.tsx
@@ -21,6 +21,14 @@ const InputWithButton = ({
 }: InputButtonProps) => {
   const [inputValue, setInputValue] = useState<string>("");
 
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleClick(inputValue);
+      setInputValue("");
+    }
+  };
+
   return (
     <InputGroup size="md">
       <Input
@@ -30,6 +38,7 @@ const InputWithButton = ({
         isDisabled={disabled}
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={handleKeyPress}
       />
       <InputRightElement width="4.5rem">
         <Button
